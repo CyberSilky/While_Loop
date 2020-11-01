8ce73829ae1d27456056482cff55835f2aeb59b6
James Shoemaker <james.shoemaker99@gmail.com>
2020-11-01 13:53:23 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+
+        while (isOnRepeat) {
+            System.out.println("Playing current song.");
+            System.out.println("Would you like to take this song off repeat? If so answer yes.");
+            String userInput = input.next();
+
+            if(userInput.equals("yes")){
+                isOnRepeat = false;
+            }
+
+        }
+        System.out.println("Playing next song.");
+    }
+}
